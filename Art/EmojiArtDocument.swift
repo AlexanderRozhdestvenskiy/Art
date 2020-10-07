17fce614271f10832e3184a0a8c2f95c9c9a2e68
AlexanderRozhdestvenskiy <alexrozh1988@gmail.com>
2020-10-07 19:16:12 +0300

    7

---
+++ Art/EmojiArtDocument.swift
@@ -0,0 +1,63 @@
+//
+//  EmojiArtDocument.swift
+//  Art
+//
+//  Created by Alexander Rozhdestvenskiy on 06.10.2020.
+//
+
+import SwiftUI
+
+class EmojiArtDocument: ObservableObject {
+    
+    static let palette: String = "🍎🐸🤿🤬🚚⌚️🖤🇷🇺"
+    
+    @Published private var emojiArt: EmojiArt = EmojiArt()
+    
+    @Published private(set) var backgroundImage: UIImage?
+    
+    var emojis: [EmojiArt.Emoji] { emojiArt.emojis }
+    
+    // MARK: - Intent(s)
+    
+    func addEmoji(_ emoji: String, at location: CGPoint, size: CGFloat) {
+        emojiArt.addEmoji(emoji, x: Int(location.x), y: Int(location.y), size: Int(size))
+    }
+
+    func moveEmoji(_ emoji: EmojiArt.Emoji, by offset: CGSize) {
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].x += Int(offset.width)
+            emojiArt.emojis[index].y += Int(offset.height)
+        }
+    }
+    
+    func scaleEmoji(_ emoji: EmojiArt.Emoji, by scale: CGFloat) {
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].size = Int((CGFloat(emojiArt.emojis[index].size) * scale).rounded(.toNearestOrEven))
+        }
+    }
+    
+    func setBackgroundURL(_ url: URL?) {
+        emojiArt.backgroundURL = url?.imageURL
+        fetchBackgroundData()
+    }
+    
+    private func fetchBackgroundData() {
+        backgroundImage = nil
+        if let url = self.emojiArt.backgroundURL {
+            DispatchQueue.global(qos: .userInitiated).async {
+                if let imageData = try? Data(contentsOf: url) {
+                    DispatchQueue.main.async {
+                        if url == self.emojiArt.backgroundURL {
+                            self.backgroundImage = UIImage(data: imageData)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension EmojiArt.Emoji {
+    var fontSize: CGFloat { CGFloat(self.size) }
+    var location: CGPoint { CGPoint(x: CGFloat(x), y: CGFloat(y)) }
+}
